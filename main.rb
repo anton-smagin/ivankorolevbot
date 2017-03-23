03a01cabb53912cb2db7357c8e3c8f8922f59e25
Anton Smagin <smagiman@gmail.com>
2017-03-23 11:35:22 +0300

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,14 +1,14 @@
 # Телеграм-бот
 
 # Этот код необходим при использовании русских букв на Windows
-if (Gem.win_platform?)
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
+# if (Gem.win_platform?)
+#   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+#   Encoding.default_internal = __ENCODING__
 
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
+#   [STDIN, STDOUT].each do |io|
+#     io.set_encoding(Encoding.default_external, Encoding.default_internal)
+#   end
+# end
 ###
 
 require 'telegram/bot'
@@ -16,32 +16,63 @@ require 'telegram/bot'
 TOKEN = '312322718:AAHmzp2WRQljmVIlh_gPKHD0TAWo4qMqpIE'#ENV['TELEGRAM_BOT_API_TOKEN']
 
 # путь к файлу с ответами
-ANSWERS_FILE_PATH = "#{File.dirname(__FILE__)}/data/answers.txt"
-
-# путь к файлу с приветствием
-GREETING_FILE_PATH = "#{File.dirname(__FILE__)}/data/greeting.txt"
-
+philosophy_path = "#{File.dirname(__FILE__)}/data/philosophy.txt"
+react_names_path = "#{File.dirname(__FILE__)}/data/react_names.txt"
+react_places_path = "#{File.dirname(__FILE__)}/data/react_places.txt"
+who_is_it_path = "#{File.dirname(__FILE__)}/data/who_is_it.txt"
+where_are_you_path = "#{File.dirname(__FILE__)}/data/where_are_you.txt"
 # открываю файл с ответами
+
 begin
-  file_answers = File.open(ANSWERS_FILE_PATH, "r:utf-8")
+  philosophy = File.open(philosophy_path, "r:utf-8")
 rescue Errno::ENOENT => e
-  puts "Файл с ответами не найден"
+  puts "Файл с философией не найден"
   abort e.message
 end
 
-answers_lines = file_answers.readlines
-file_answers.close
+philosophy_lines = philosophy.readlines
+philosophy.close
 
 # открываю файл с приветствием
 begin
-  file_greeting = File.open(GREETING_FILE_PATH, "r:utf-8")
+  react_names = File.open(react_names_path, "r:utf-8")
 rescue Errno::ENOENT => e
-  puts "Файл с ответами не найден"
+  puts "Файл с именами не найден"
   abort e.message
 end
 
-greeting_lines = file_greeting.readlines
-file_greeting.close
+react_names_lines = react_names.readlines
+react_names.close
+
+begin
+  react_places = File.open(react_places_path, "r:utf-8")
+rescue Errno::ENOENT => e
+  puts "Файл с именами не найден"
+  abort e.message
+end
+
+react_places_lines = react_places.readlines
+react_places.close
+
+begin
+  who_is_it = File.open(who_is_it_path, "r:utf-8")
+rescue Errno::ENOENT => e
+  puts "Файл с именами не найден"
+  abort e.message
+end
+
+who_is_it_lines = who_is_it.readlines
+who_is_it.close
+
+begin
+  where_are_you = File.open(where_are_you_path, "r:utf-8")
+rescue Errno::ENOENT => e
+  puts "Файл с именами не найден"
+  abort e.message
+end
+
+where_are_you_lines = where_are_you.readlines
+where_are_you.close
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
@@ -53,17 +84,20 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
                 "Можешь задать мне любой интересующий тебя вопрос..."
         )
       else
+        react_names_lines.each do |name|
+          if message.text.downcase.include? name
+              bot.api.send_message(chat_id: message.chat.id, text: who_is_it_lines.sample) 
+            break
+          end
+        end
+        react_places_lines.each do |place|
+          if message.text.downcase.include? place
+              bot.api.send_message(chat_id: message.chat.id, text: where_are_you_lines.sample) 
+            break
+          end
+        end
         sleep 1
-        bot.api.send_message(
-            chat_id: message.chat.id,
-            text: greeting_lines.sample
-        )
-
-        sleep 3
-        bot.api.send_message(
-            chat_id: message.chat.id,
-            text: answers_lines.sample
-        )
+        bot.api.send_message(chat_id: message.chat.id, text: philosophy.sample) if ( rand(0..100) > 1 )      
     end
   end
 end
